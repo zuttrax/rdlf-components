42a93137a681a455d919531d630d7eabd243f127
Angel Zambrano <angel.zneira@gmail.com>
2020-03-24 16:45:27 -0300

    Add package for logs
    Add new component for write logs in rudolf applications, this component
    is an abstrabtion of uber logs package (zap)

---
+++ pkg/logs/logger.go
@@ -0,0 +1,72 @@
+package logs
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+type Log struct {
+	AppName string
+}
+
+type Logger interface {
+	Info()
+	Error()
+	Fatal()
+}
+
+func (l Log) Info(info string) {
+	logger := initialConfiguration(l.AppName)
+	defer logger.Sync()
+	logger.Info(info)
+
+}
+
+func (l Log) Error(err error) {
+	logger := initialConfiguration(l.AppName)
+	defer logger.Sync()
+	logger.Error(err.Error())
+
+}
+
+func (l Log) Fatal(err error) {
+	logger := initialConfiguration(l.AppName)
+	defer logger.Sync()
+	logger.Fatal(err.Error())
+
+}
+
+func initialConfiguration(app string) *zap.Logger {
+	date := time.Now().Format("01-02-2006")
+	outFile := fmt.Sprintf("/rdlf/%s-%s.log", app, date)
+	rawJSON := []byte(`{
+	  "level": "debug",
+	  "encoding": "json",
+	  "outputPaths": ["stdout",` + outFile + `],
+	  "errorOutputPaths": ["stderr"],
+	  "encoderConfig": {
+	    "messageKey": "message",
+	    "levelKey": "level",
+		"levelEncoder": "lowercase",
+		"timeKey": "time",
+	    "timeEncoder": "ISO8601"
+	  }
+	}`)
+
+	var cfg zap.Config
+
+	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+		panic(err)
+	}
+
+	logger, err := cfg.Build()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return logger
+}
